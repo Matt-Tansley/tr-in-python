b318c9ab9a0e4cacf8b784af9d4bf05f17676ee8
CrunchyPancakes <30matthewt@gmail.com>
2024-01-01 21:41:11 +1300

    Working function for: - Replacing a specific string with a specific string. - Replacing a range of characters with a specific string.

--- tr.py
+++ tr.py
@@ -2,17 +2,44 @@
 
 import argparse
 
+
+# Helper functions
+def tr_replace(text, str1, str2):
+    new_text = text
+
+    if is_range(str1):
+        for c in char_range(str1[0], str1[2]):
+            new_text = new_text.replace(c, str2)
+    else:
+        new_text = new_text.replace(str1, str2)
+
+    return new_text
+
+
+def char_range(c1, c2):
+    """Generates the characters from `c1` to `c2`, inclusive."""
+    for c in range(ord(c1), ord(c2) + 1):
+        yield chr(c)
+
+
+def is_range(string: str):
+    return (len(string) == 3
+            and string[0].isalpha()
+            and string[1] == "-"
+            and string[2].isalpha())
+
+
 # argparse setup
 help_description = "Adding description"
 parser = argparse.ArgumentParser(description=help_description)
 
 parser.add_argument("-f", "--fun",
-                    help = "Have some fun",
-                    action = "store_true")
+                    help="Have some fun",
+                    action="store_true")
 
 parser.add_argument("strings",
-                    help = "The original string to search for",
-                    nargs = "+")
+                    help="The original string to search for",
+                    nargs="+")
 
 # Read args from command line
 args = parser.parse_args()
@@ -32,10 +59,11 @@ if len(strings) > 2:
     quit()
 elif len(strings) > 1:
     # Step 1: Replace one character with another
-    text = input("Enter text input: ")
-    print("Text input is: " + text)
+    text = input("\nEnter text input: ")
+    print("\nText input is: " + text)
 
-    new_text = text.replace(strings[0], strings[1])
-    print(new_text)
+    new_text = tr_replace(text, strings[0], strings[1])
+
+    print("\nText output is: " + new_text)
 else:
     print("No implemention for 1 or 0 strings yet")
